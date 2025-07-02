abcd29f69b70f74eefb9a5f5cc8f09ab7e064ab9
unknown <scandiclamar@gmail.com>
2025-07-02 15:28:52 +0000

    testing-build

--- sec_frontend/src/components/api.ts
+++ sec_frontend/src/components/api.ts
@@ -1,7 +1,7 @@
-const baseUrl = import.meta.env.VITE_API_BASE_URL || 'https://sec-insights-backend-791634680391.us-central1.run.app';
-export default baseUrl;
-const baseUrl2 = 'http://localhost:8000';   
-console.log("Using baseUrl:", baseUrl);
-console.log("Using baseUrl2:", baseUrl2);
+const baseUrl =
+  window.location.hostname === 'localhost'
+    ? 'http://localhost:8000'
+    : 'https://sec-insights-backend-791634680391.us-central1.run.app';
 
-export { baseUrl2 };
+console.log('Using baseUrl:', baseUrl);
+export default baseUrl;
