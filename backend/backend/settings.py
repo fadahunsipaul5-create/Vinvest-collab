51d54b65cfd0becee6503f2aa93b71ebe85de3cc
Fadahunsi Paul <paul@nanikworkforce.com>
2025-07-24 11:58:45 +0000

    new

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 from datetime import timedelta
-
+#comment
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
