d94664bdb1fa96923cf8e26fe9316b129207269e
Fadahunsi Paul <scandiclamar@gmail.com>
2025-08-01 09:59:21 -0700

    new build

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,7 +33,7 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
+        'HOST': os.environ.get('DB_HOST','127.0.0.1'),
         'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
