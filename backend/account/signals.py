c8b55ac89f9323699070248b376ebea13ddf674e
Fadahunsi Paul <scandiclamar@gmail.com>
2025-08-13 12:52:25 +0000

    fixed register issues

--- backend/account/signals.py
+++ backend/account/signals.py
@@ -1,18 +1,3 @@
-from django.conf import settings
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-from django.contrib.auth import get_user_model
-User = get_user_model()
-
-from .models import User
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        User.objects.get_or_create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    # ensures profile exists and saves it (safe if profile is already there)
-    User.objects.get_or_create(user=instance)
+# Signals removed - not needed since we store all user data directly in the User model
+# The previous signals were trying to create additional User objects which caused
+# database errors when registering new users.
